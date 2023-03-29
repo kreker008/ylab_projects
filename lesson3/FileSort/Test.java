fdd9b4f69c27696771db705556c4e50583d18bc0
Vagarshak <71036168+kreker008@users.noreply.github.com>
2023-03-29 14:49:20 +0400

    Update Test.java

--- lesson3/FileSort/Test.java
+++ lesson3/FileSort/Test.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        File dataFile = new Generator().generate("data.txt", 1000);
+        File dataFile = new Generator().generate("data.txt", 50000);
         System.out.println(new Validator(dataFile).isSorted()); // false
         File sortedFile = new Sorter().sortFile(dataFile);
         System.out.println(new Validator(sortedFile).isSorted()); // true
